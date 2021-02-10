8872ff4f5fefa16cd027d08fa165ca603b30b8a6
Karl Mabunga <karlmabunga@yahoo.com>
2021-02-09 18:20:00 -0800

    Implemented LinkedList with get, at, compare, swap, and print methods.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([10, 3,-5, 0]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.bubbleSort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('GHIbcaEf');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('GHIbcaEf');
+// const sorter = new Sorter(charactersCollection);
+// sorter.bubbleSort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-100);
+linkedList.add(300);
+
+const sorter = new Sorter(linkedList);
 sorter.bubbleSort();
-console.log(charactersCollection.data);
+linkedList.print()
